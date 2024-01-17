f0f771aea2dcc0560e9e8f4abfa46e778878c249
Adrian <adrianbueno095@gmail.com>
2024-01-17 12:44:04 +0100

    post, get, getById

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const app = express();
 const MONGODB_URI = "mongodb://127.0.0.1:27017/express-mongoose-recipes-dev";
-
+const Recipe = require("./models/Recipe.model")
 // MIDDLEWARE
 app.use(logger("dev"));
 app.use(express.static("public"));
@@ -12,15 +12,12 @@ app.use(express.json());
 
 // Iteration 1 - Connect to MongoDB
 // DATABASE CONNECTION
-
-
 mongoose
     .connect(MONGODB_URI)
     .then((x) => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
     .catch((err) => console.error("Error connecting to mongo", err));
 
 // ...
-
 
 // ROUTES
 //  GET  / route - This is just an example route
@@ -31,15 +28,33 @@ app.get('/', (req, res) => {
 
 //  Iteration 3 - Create a Recipe route
 //  POST  /recipes route
-
+app.post('/recipes', async (request, response) => {
+    // Get back the data from the request
+    console.log(request.body)
+    const payload = request.body
+    try {
+        const newRecipe = await Recipe.create(payload)
+        // return the new Recipe into the response
+        response.status(201).json(newRecipe)
+    } catch (error) {
+        response.status(500).json({ error, message: 'Error while creating new Recipe' })
+    }
+})
 
 //  Iteration 4 - Get All Recipes
 //  GET  /recipes route
-
-
+app.get('/recipes', async (request, response) => {
+    const recipes = await Recipe.find()
+    response.json(recipes)
+})
 //  Iteration 5 - Get a Single Recipe
 //  GET  /recipes/:id route
+app.get('/recipes/:recipeId', async (request, response) => {
+    const { recipeId } = request.params
+    const oneRecipe = await Recipe.findById(recipeId)
 
+    response.json(oneRecipe)
+})
 
 //  Iteration 6 - Update a Single Recipe
 //  PUT  /recipes/:id route
